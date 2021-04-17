400bf06b49e509ddbbc0fb87ac01f31982f0504b
Leonard Avila <lavila@ecomsur.com>
2021-04-17 17:08:54 -0400

    update

--- pages/_document.js
+++ pages/_document.js
@@ -13,13 +13,25 @@ class MyDocument extends Document {
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>Leonard J. Ávila S. | Front-end Engineer</title>
+          {/* Primary Meta Tags */}
+          <meta name="title" content="Leonard J. Ávila S. | Front-end Engineer"/>
+          <meta name="description" content="Web Development, Javascript, React,  Next, Node, CSS ,Web Performance."/>
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website">
+          <meta property="og:url" content="https://www.leonardavila.dev/"/>
+          <meta property="og:title" content="Leonard J. Ávila S. | Front-end Engineer"/>
+          <meta property="og:description" content="Web Development, Javascript, React,  Next, Node, CSS ,Web Performance.">
+          <meta property="og:image" content="../public/developer.png">
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image">
+          <meta property="twitter:url" content="https://www.leonardavila.dev/"/>
+          <meta property="twitter:title" content="Leonard J. Ávila S. | Front-end Engineer"/>
+          <meta property="twitter:description" content="Web Development, Javascript, React,  Next, Node, CSS ,Web Performance."/>
+          <meta property="twitter:image" content="../public/developer.png"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-        </Head>
-        <Head>
-          <meta property="og:title" content="My new title" key="title" />
         </Head>
         <body className="Santiago">
           <Main />
@@ -31,3 +43,6 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
+
+
