5942b3ca6df7b578fda1cc8ef08cbaec9df9ee30
Leonard Avila <lavila@ecomsur.com>
2021-04-17 17:38:54 -0400

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -8,13 +8,15 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="es">
         <Head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>Leonard J. Ávila S. | Front-end Engineer</title>
           <meta name="title" content="Leonard J. Ávila S. | Front-end Engineer" />
           <meta name="description" content="Web Development, Javascript, React, Next, Node, Web Performance, WordPress, Front-end, Back-end, CSS preprocessors, Frameworks CSS." />
+          <meta name="author" content="Leonard J. Ávila S." />
+          <meta name="keywords" content="Front-end, Javascript, React" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
